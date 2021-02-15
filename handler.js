885b6091a1ef12ff4f487c377fdcee9f8a4e7398
Frank Liardet <frank.liardet@gmail.com>
2021-02-16 08:47:37 +1100

    getSecurityGroups lambda working

--- handler.js
+++ handler.js
@@ -1,18 +1,31 @@
 'use strict';
+const AWS = require('aws-sdk');
+require('dotenv').config();
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+AWS.config.update({ region: 'us-east-1' });
+const ec2 = new AWS.EC2();
+
+module.exports.getSecurityGroups = async (event) => {
+  const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' });
+
+  const params = {
+    GroupIds: [process.env.AWS_SECURITY_GROUP_ID_1, process.env.AWS_SECURITY_GROUP_ID_2],
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  try {
+    const response = await ec2.describeSecurityGroups(params).promise();
+    console.log('response: ', response);
+    // I'm returning the group names only for security reasons since I'm new to serverless and
+    // can't be sure of all the security precautions required to protect my data
+    const securityGroups = response.SecurityGroups.map((group) => group.GroupName);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(securityGroups),
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ 'Error: ': err }),
+    };
+  }
 };
